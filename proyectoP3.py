7589750896b2fe6b2188da75926f0fc6b8352676
Maria Estrada <m.estradag@uniandes.edu.co>
2022-12-07 23:40:22 -0500

    Commit entrega 3

--- proyectoP3.py
+++ proyectoP3.py
@@ -1,29 +1,38 @@
-
 import sys
 
 
-def salto_robot(m:int, k:int):
+def saltoDivisorRobotVAMOSVENCIDA(m, k):
+    k_inicial = k
     if k > m:
         return 0
     else:
-        matrix = [[0] * (m+1) for i in range(0, m)]
-        matrix_conect = [[0] * (m+1) for i in range(0, m)]
-        for i in range(0, m):
-            for j in range(0,m+1):
-                diff = (j-i)%k
-                if i == 0 and diff == 0:
-                    matrix[i][j] = 1
-                elif i == j:
-                    matrix[i][j] = 0
-                elif k >= j or diff !=0:
-                    matrix[i][j] = matrix[i-1][j]
-                else:
-                    matrix[i][j] = matrix[i-1][j] + matrix[i-1][i]
-            k= (k+1)%998244353
-        return matrix
-m= salto_robot(7,1)
-for p in m:
-    print(p)
+        matrix = [[0] * (m+1) for i in range(0, m-k_inicial+1)]
+        for x in range(0, len(matrix[0])):
+            if x%k_inicial == 0:
+                    matrix[0][x] = 1
+
+        #k += 1
+        for i in range(0, m+1):
+            k = (k+1)%998244353
+            for j in range(0,m-k_inicial+1):
+                if i == j:
+                    matrix[i][j] = 0 #Diagonal principal
+                if j > i and matrix[i][j] > 0 and i + 1 < m and j + k < len(matrix[0]) :
+                     matrix[i+1][j + k] = (matrix[i+1][j + k] + matrix[i][j])%998244353
+                z = j
+                while z + k + k  < len(matrix[0]):
+                    z += k    
+                    matrix[i+1][z+k] =  (matrix[i+1][z+k] + matrix[i][j])%998244353
+
+        return sumUltimaColumna(matrix)
+
+def sumUltimaColumna(matrix):
+    suma = 0
+    for i in range(0, len(matrix)):
+        suma += matrix[i][len(matrix[0])-1]
+    return suma
+
+
 """numero_casos = int(sys.stdin.readline())
 for __ in range(numero_casos):
     caso = list(map(int, sys.stdin.readline().split()))
@@ -33,3 +42,6 @@ for __ in range(numero_casos):
         print(salto_robot(m, k))
     else:
         print('Error en los parametros iniciales')"""
+
+m= saltoDivisorRobotVAMOSVENCIDA(24,1)
+print(m)
